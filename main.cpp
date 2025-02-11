80cf0340d84b5b7728673bc5356109bafe250bdb
Keillor Jennings <69380836+keillor@users.noreply.github.com>
2025-02-10 18:25:38 -0800

    created rat25 source, and dummy main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "la.hpp"
 
-#include "la.h"
+#define FILE_NAME "source.rat25s"
+
+Token lexer(std::string unparsedText) {
+    return Token(TokenType::KEYWORD, "DUMMY TOKEN");
+}
 
 int main() {
-    std::cout << "Lexical Analyzer Running..." << std::endl;
+    std::string line;
+    std::fstream sourceCodeFile;
 
-    // place holder
-    LexicalAnalyzer lexer;
-    std::string input = "int x = 10;";
-    lexer.tokenize(input);
-    lexer.printTokens();
+    sourceCodeFile.open(FILE_NAME);
 
+    std::cout << "Token" << "\t\t\t" << "lexeme" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+
+    while (std::getline(sourceCodeFile, line)) {
+        Token parsedToken = lexer(line);
+        std::cout << parsedToken.value << "\t\t" << parsedToken.type << std::endl;
+        //call lexer for a token
+        //print:
+        //      token 
+        //      lexeme
+    }
+
+    sourceCodeFile.close();
     return 0;
 }
